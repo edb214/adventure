5bc4113a4c8f2b87eee001de378d1bb04fc01c2c
edb214 <47918410+edb214@users.noreply.github.com>
2019-02-23 15:42:53 +0000

    Testing pygame

---
+++ main.py
@@ -0,0 +1,30 @@
+# Test project
+
+# import the pygame module, so you can use it
+import pygame
+
+# define a main function
+xpos = 0
+ypos = 0
+step_x = 1
+step_y = 1
+
+pygame.init()
+
+screen = pygame.display.set_mode((1000,750))
+image = pygame.image.load("alex.jpg")  
+screen.fill((100,8,60)) 
+pygame.display.flip()
+
+logo = pygame.image.load("ham.jpg")
+pygame.display.set_icon(logo)
+pygame.display.set_caption("This is a screen")
+    
+for i in range(400):
+    xpos += step_x # move it to the right
+    ypos += step_y # move it down    
+    screen.fill((100,8,60))
+    screen.blit(image, (xpos, ypos))
+    pygame.display.flip()
+while True:
+    pygame.display.flip()
